f3403f8144403c1e5704ea7d3158729565caa33c
Mario Reder <mario.reder@pm.me>
2025-10-08 09:42:31 +0200

    Setup DEX with workflow files and configuration

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__RUNTIME_CONFIG__ = {
-  "VITE_ORDERLY_BROKER_ID": "demo",
+  "VITE_ORDERLY_BROKER_ID": "novex_fi",
   "VITE_ORDERLY_BROKER_NAME": "Novex Finance",
   "VITE_DISABLE_MAINNET": "false",
   "VITE_DISABLE_TESTNET": "false",
